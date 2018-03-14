3aef518860f2018d4a90f57dfd34894d65a5df7b
David García <ccristhiangarcia@gmail.com>
2018-03-14 16:43:03 -0500

    Install Laravel Debug Bar Component

--- config/app.php
+++ config/app.php
@@ -141,6 +141,7 @@ return [
          * Vendor Service Providers...
          */
         Collective\Html\HtmlServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -200,6 +201,7 @@ return [
 
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 
 ];
